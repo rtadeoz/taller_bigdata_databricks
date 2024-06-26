1dcd71cafb5ed95ec8191bdfddeb74ae136aaedd
Richard Tadeo <rickt89@gmail.com>
2024-06-25 19:06:54 -0500

    Actualizacion rutas

--- DATAMART_VENTAS/dataops/Jenkins/Jenkinsfile_desa.groovy
+++ DATAMART_VENTAS/dataops/Jenkins/Jenkinsfile_desa.groovy
@@ -34,11 +34,11 @@ node {
     
     stage('Deploy in Azure Databricks') {
     echo "Despliegue en Databricks"
-    sh "databricks workspace mkdirs /prod/datamart_ventas/ --profile JENKINS"
-    sh "databricks workspace import /prod/datamart_ventas/01_Bronze.py --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/01_Bronze.py  --format SOURCE --language PYTHON --profile JENKINS"       
-    sh "databricks workspace import /prod/datamart_ventas/02_Silver.sql --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/02_Silver.sql  --format SOURCE --language SQL --profile JENKINS"      
-    sh "databricks workspace import /prod/datamart_ventas/03_Gold.sql --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/03_Gold.sql  --format SOURCE --language SQL --profile JENKINS"      
-    sh "databricks workspace import /prod/datamart_ventas/00_Montar_Sistema_Archivos.scala --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/00_Montar_Sistema_Archivos.scala  --format SOURCE --language SCALA --profile JENKINS"      
+    sh "databricks workspace mkdirs /prod/corp/finanzas/datamart_ventas/ --profile JENKINS"
+    sh "databricks workspace import /prod/corp/finanzas/datamart_ventas/01_Bronze.py --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/01_Bronze.py  --format SOURCE --language PYTHON --profile JENKINS"       
+    sh "databricks workspace import /prod/corp/finanzas/datamart_ventas/02_Silver.sql --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/02_Silver.sql  --format SOURCE --language SQL --profile JENKINS"      
+    sh "databricks workspace import /prod/corp/finanzas/datamart_ventas/03_Gold.sql --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/03_Gold.sql  --format SOURCE --language SQL --profile JENKINS"      
+    sh "databricks workspace import /prod/corp/finanzas/datamart_ventas/00_Montar_Sistema_Archivos.scala --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/00_Montar_Sistema_Archivos.scala  --format SOURCE --language SCALA --profile JENKINS"      
 	}
    
 }
