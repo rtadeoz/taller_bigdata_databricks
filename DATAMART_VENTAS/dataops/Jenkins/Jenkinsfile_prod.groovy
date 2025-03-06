d2a3c98d4a2421b56869bb4c22b92aa3a545af1d
rtadeoz <51861582+rtadeoz@users.noreply.github.com>
2025-03-05 20:57:26 -0500

    Update Jenkinsfile_prod.groovy

--- DATAMART_VENTAS/dataops/Jenkins/Jenkinsfile_prod.groovy
+++ DATAMART_VENTAS/dataops/Jenkins/Jenkinsfile_prod.groovy
@@ -35,10 +35,10 @@ node {
     stage('Deploy in Azure Databricks') {
     echo "Despliegue en Databricks"
     sh "databricks workspace mkdirs /${PRM_AMBIENTE}/corp/finanzas/datamart_ventas/ --profile JENKINS"
-    sh "databricks workspace import /${PRM_AMBIENTE}/corp/finanzas/datamart_ventas/01_Bronze.py --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/01_Bronze.py  --format SOURCE --language PYTHON --profile JENKINS"       
-    sh "databricks workspace import /${PRM_AMBIENTE}/corp/finanzas/datamart_ventas/02_Silver.sql --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/02_Silver.sql  --format SOURCE --language SQL --profile JENKINS"      
-    sh "databricks workspace import /${PRM_AMBIENTE}/corp/finanzas/datamart_ventas/03_Gold.sql --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/03_Gold.sql  --format SOURCE --language SQL --profile JENKINS"      
-    sh "databricks workspace import /${PRM_AMBIENTE}/corp/finanzas/datamart_ventas/00_Montar_Sistema_Archivos.scala --file ${workspace}/DATAMART_VENTAS/src/deploy/dbfs/00_Montar_Sistema_Archivos.scala  --format SOURCE --language SCALA --profile JENKINS"      
+    sh "databricks workspace import /${PRM_AMBIENTE}/corp/finanzas/datamart_ventas/01_Bronze.py --file ${workspace}/DATAMART_VENTAS/src/deploy/adb/01_Bronze.py  --format SOURCE --language PYTHON --profile JENKINS"       
+    sh "databricks workspace import /${PRM_AMBIENTE}/corp/finanzas/datamart_ventas/02_Silver.sql --file ${workspace}/DATAMART_VENTAS/src/deploy/adb/02_Silver.sql  --format SOURCE --language SQL --profile JENKINS"      
+    sh "databricks workspace import /${PRM_AMBIENTE}/corp/finanzas/datamart_ventas/03_Gold.sql --file ${workspace}/DATAMART_VENTAS/src/deploy/adb/03_Gold.sql  --format SOURCE --language SQL --profile JENKINS"      
+    sh "databricks workspace import /${PRM_AMBIENTE}/corp/finanzas/datamart_ventas/00_Montar_Sistema_Archivos.scala --file ${workspace}/DATAMART_VENTAS/src/deploy/adb/00_Montar_Sistema_Archivos.scala  --format SOURCE --language SCALA --profile JENKINS"      
     }
    
 }
